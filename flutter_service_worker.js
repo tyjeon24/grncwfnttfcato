46c2e0a573dabcf1f01f9afa8ad19398ad2dbbe2
tyjeon24 <tyjeon24@users.noreply.github.com>
2022-12-21 17:17:42 +0000

    deploy: ec846cfe29f56f6a10061fe387c1304f3e8f5f4f

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -42,8 +42,8 @@ const RESOURCES = {
 "assets/assets/txt/HoldingTax_description.txt": "dd1f7922ffccd98480eed6e310ddb970",
 "assets/assets/txt/HoldingTax_precaution.txt": "3ae2978170febf9a110bf6c057e05c26",
 "assets/NOTICES": "2f60d974f03c04fdad1a35d374cc832f",
-"index.html": "1870cf9e771c2369ba87c1d0eff915bc",
-"/": "1870cf9e771c2369ba87c1d0eff915bc",
+"index.html": "5ee86051caa55175bffb69c4d8ef4fdd",
+"/": "5ee86051caa55175bffb69c4d8ef4fdd",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "12385392cb97fc7e83d60845948177b1"
 };
